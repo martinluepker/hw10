1461f62f6b58b3f57099fa959a73a9db1d02690a
cheasify <shrimpyelves@gmial.com>
2015-12-09 00:18:30 -0600

    clear

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main()
 	bool driving=true;
 	while(driving)
 	{
-	
+	  r.placeObj(Road::INIT_WIDTH,0,' ');//clears
 	  c.amble(); //move the car around
 	  if(rand() % 20 == 1)//5% chance to have pedestrian with 
 	    {
