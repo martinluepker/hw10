4a3e20be3d9be8af9bedecf0bc5b85a56c2e0b9e
ben krueger <bokkw4@mst.edu>
2015-12-09 00:11:43 -0600

    fucked it

--- main.cpp
+++ main.cpp
@@ -21,68 +21,68 @@ int main()
 	while(driving)
 	{
 	
-	
-	if(rand() % 20 == 1)//5% chance to have pedestrian with 
-	{
-	  Pedestrian p;
-	  cout<<"\nA pedestrian appears.\n";
-	  while(p.getPos()< Road::INIT_WIDTH)
-	  {
+	  c.amble(); //move the car around
+	  if(rand() % 20 == 1)//5% chance to have pedestrian with 
+	    {
+	      Pedestrian p;
+	      cout<<"\nA pedestrian appears.\n";
+	      while(p.getPos()< Road::INIT_WIDTH)
+		{
 
-	    p.walk(r,c);
-	    r.placeObj(1,p.getPos(),'p');
-	    cout <<r;
-	  }
-	}
-	else//if no pedestrian in street have animal try to cross
-	{
-	  int animalIndex = rand() % NUM_ANIMALS;
-	  if(animals[animalIndex].chooseToRun(rand() % STUPID_LEVELS))
-	  {
-	    animals[animalIndex].setPos(rand() % Road::INIT_WIDTH);
+		  p.walk(r,c);
+		  r.placeObj(1,p.getPos(),'p');
+		  cout <<r;
+		}
+	    }
+	  else//if no pedestrian in street have animal try to cross
+	    {
+	      int animalIndex = rand() % NUM_ANIMALS;
+	      if(animals[animalIndex].chooseToRun(rand() % STUPID_LEVELS))
+		{
+		  animals[animalIndex].setPos(rand() % Road::INIT_WIDTH);
 	    
-	  }
+		}
 	  
-	  //COLLISIONS PART
-	  if(animals[animalIndex].getPos() >= c.getPos() || 
-	     animals[animalIndex].getPos() +animals[animalIndex].getWidth() <= c.getPos()+ c.getWidth())
-	    // if hit add dammage and battery switch animal hit bool
-	  {
-	    c.incrDamage(animals[animalIndex].getWeight());
-	    c.incrBattery(animals[animalIndex].getWeight());
-	    animals[animalIndex].hit();
-	    // say comment
-	    lonestar(animals[animalIndex]);
-	    // add escore
-	    c.incrEscore(animals[animalIndex].getWeight());
-	    r.placeObj(animals[animalIndex].getWidth(),animals[animalIndex].getPos(),'a');
-	  }
+	      //COLLISIONS PART
+	      if(animals[animalIndex].getPos() >= c.getPos() || 
+		 animals[animalIndex].getPos() +animals[animalIndex].getWidth() <= c.getPos()+ c.getWidth())
+		// if hit add dammage and battery switch animal hit bool
+		{
+		  c.incrDamage(animals[animalIndex].getWeight());
+		  c.incrBattery(animals[animalIndex].getWeight());
+		  animals[animalIndex].hit();
+		  // say comment
+		  lonestar(animals[animalIndex]);
+		  // add escore
+		  c.incrEscore(animals[animalIndex].getWeight());
+		  r.placeObj(animals[animalIndex].getWidth(),animals[animalIndex].getPos(),animals[animalIndex].getSymbol());
+		}
 	  
-	}
+	    }
        	
-	c.changeBattery(-1);//sub battery
-	dist++;
-	c.amble();
-	//Output road state
-	cout<<r;
-	r.placeObj(c.getWidth(),c.getPos(),'c');
-	//end conds
-	if(c.getDamage() > Car::DAMAGE_MAX)	//damage 
-	  {
-	    cout<<"\nCar too damaged to go on. Exiting.\n";
-	    driving = false;
-	  }	
-	else if(dist >= MAX_DIST)//dist
-	  {
-	    cout<<"\nCar has completed \n";
-	    driving =false;
-	  }
-	else if(c.getBattery() <= 0)//battery empty
-	  {
-	    cout<<"\nBattery ran out.\n";
-	    driving = false;
-	  }
-	  
+	  c.changeBattery(-1);//sub battery
+	  dist++;
+	
+	  //Output road state
+	  cout<<r;
+	  r.placeObj(c.getWidth(),c.getPos(),'c');
+	  //end conds
+	  if(c.getDamage() > Car::DAMAGE_MAX)	//damage 
+	    {
+	      cout<<"\nCar too damaged to go on. Exiting.\n";
+	      driving = false;
+	    }	
+	  else if(dist >= MAX_DIST)//dist
+	    {
+	      cout<<"\nCar has completed \n";
+	      driving =false;
+	    }
+	  else if(c.getBattery() <= 0)//battery empty
+	    {
+	      cout<<"\nBattery ran out.\n";
+	      driving = false;
+	    }
+	 
 	}//while
 	//print 
 		//list of animals hit
